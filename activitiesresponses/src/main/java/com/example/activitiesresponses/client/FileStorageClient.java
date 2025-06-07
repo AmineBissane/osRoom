f499b95aafdd365f94c5046922efa5ad07e904f1
Amine Bissane <122304331+AmineBissane@users.noreply.github.com>
2025-06-07 16:30:39 +0200

    fix file upload

--- activitiesresponses/src/main/java/com/example/activitiesresponses/client/FileStorageClient.java
+++ activitiesresponses/src/main/java/com/example/activitiesresponses/client/FileStorageClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "file-storage", url = "${app.file-storage.url:http://82.29.168.17:8090}", path = "/api/v1/file-storage")
+@FeignClient(name = "file-storage", url = "${app.file-storage.url:http://82.29.168.17:8030}", path = "/api/v1/file-storage")
 public interface FileStorageClient {
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
